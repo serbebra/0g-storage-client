0c434264d15d05f9c1f5bbaaa9444606a2b35f0c
sergo <154074169+serbebra@users.noreply.github.com>
2024-05-27 03:12:25 +0200

    Update deploy.go

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,6 +4,8 @@ import (
 	"github.com/0glabs/0g-storage-client/common/blockchain"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"strings"
 )
 
 var (
@@ -22,22 +24,45 @@ var (
 
 func init() {
 	deployCmd.Flags().StringVar(&deployArgs.url, "url", "", "Fullnode URL to interact with blockchain")
-	deployCmd.MarkFlagRequired("url")
+	if err := deployCmd.MarkFlagRequired("url"); err != nil {
+		logrus.WithError(err).Fatal("Failed to mark 'url' flag as required")
+	}
 	deployCmd.Flags().StringVar(&deployArgs.key, "key", "", "Private key to create smart contract")
-	deployCmd.MarkFlagRequired("key")
-	deployCmd.Flags().StringVar(&deployArgs.bytecodeOrFile, "bytecode", "", "ZeroGStorage smart contract bytecode")
-	deployCmd.MarkFlagRequired("bytecode")
+	if err := deployCmd.MarkFlagRequired("key"); err != nil {
+		logrus.WithError(err).Fatal("Failed to mark 'key' flag as required")
+	}
+	deployCmd.Flags().StringVar(&deployArgs.bytecodeOrFile, "bytecode", "", "ZeroGStorage smart contract bytecode or path to bytecode file")
+	if err := deployCmd.MarkFlagRequired("bytecode"); err != nil {
+		logrus.WithError(err).Fatal("Failed to mark 'bytecode' flag as required")
+	}
 
+	// Ensure rootCmd is properly initialized
+	if rootCmd == nil {
+		logrus.Fatal("rootCmd is not initialized")
+	}
 	rootCmd.AddCommand(deployCmd)
 }
 
-func deploy(*cobra.Command, []string) {
+func deploy(cmd *cobra.Command, args []string) {
 	client := blockchain.MustNewWeb3(deployArgs.url, deployArgs.key)
 
-	contract, err := blockchain.Deploy(client, deployArgs.bytecodeOrFile)
+	bytecode := deployArgs.bytecodeOrFile
+	if isFile(bytecode) {
+		data, err := ioutil.ReadFile(bytecode)
+		if err != nil {
+			logrus.WithError(err).Fatal("Failed to read bytecode file")
+		}
+		bytecode = string(data)
+	}
+
+	contract, err := blockchain.Deploy(client, bytecode)
 	if err != nil {
 		logrus.WithError(err).Fatal("Failed to deploy smart contract")
 	}
 
 	logrus.WithField("contract", contract).Info("Smart contract deployed")
+}
+
+func isFile(path string) bool {
+	return strings.HasSuffix(path, ".bin") || strings.HasSuffix(path, ".sol")
 }
